9e98f077837b5f51855982f2ed39c6dbb96d3e74
Parhelion <kiralnm2@gmail.com>
2024-03-24 20:14:22 +0900

    Toon Shader and Outline Shader

---
+++ Plugins/ALS-Refactored-ue-5.2/Source/ALS/Private/Settings/AlsAnimationInstanceSettings.cpp
@@ -0,0 +1,26 @@
+﻿#include "Settings/AlsAnimationInstanceSettings.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AlsAnimationInstanceSettings)
+
+UAlsAnimationInstanceSettings::UAlsAnimationInstanceSettings()
+{
+	InAir.GroundPredictionResponseChannels =
+	{
+		ECC_WorldStatic,
+		ECC_WorldDynamic,
+		ECC_Destructible
+	};
+
+	InAir.GroundPredictionSweepResponses.WorldStatic = ECR_Block;
+	InAir.GroundPredictionSweepResponses.WorldDynamic = ECR_Block;
+	InAir.GroundPredictionSweepResponses.Destructible = ECR_Block;
+}
+
+#if WITH_EDITOR
+void UAlsAnimationInstanceSettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	InAir.PostEditChangeProperty(PropertyChangedEvent);
+
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+}
+#endif
